eec14e42b19a346a69e0a818fa5e2f3e8cb4584a
Nix151515 <nicolae_state_2009@yahoo.com>
2018-04-15 03:45:14 +0300

    cacat

--- src/app/mainPage/nav-bar/nav-bar.component.ts
+++ src/app/mainPage/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../../services/search.service';
+import { QueryParamsService } from '../../services/query-params.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  done = false;
+  days=[];
+  constructor(private getW:SearchService,private qu : QueryParamsService) { 
+    qu.statusUpdated.subscribe((q)=>{
+      getW.getW(q).subscribe((res)=>{
+          this.days[0]=res.temperature.substring(21, res.temperature.len);
+          this.days[1]=res.forecast.Day2
+          this.days[2]=res.forecast.Day3
+          console.log(res);
+          this.done = true;
+      })
+    })
+ 
+  }
 
-  constructor() { }
 
-  ngOnInit() {
+
+  ngOnInit() { 
   }
 
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -4,12 +4,15 @@ import 'rxjs/Rx';
 @Injectable()
 export class SearchService {
   url = 'http://find-me-some-hoes.herokuapp.com/mizerii'
+  urlw = 'http://find-me-some-hoes.herokuapp.com/weather'
   constructor(private http: Http) { }
 
   getResults(query){
-
     return this.http.post(this.url,query).map(res => res.json());
-    
+  }
+
   
+  getW(query){
+    return this.http.post(this.urlw,query).map(res => res.json());
   }
 }
